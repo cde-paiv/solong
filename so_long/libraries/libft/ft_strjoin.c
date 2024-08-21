c9d5b0095ef53b20b4c77cac0d459e7c29165d8e
Christofer Mota <155578427+cde-paiv@users.noreply.github.com>
2024-08-21 20:44:30 +0100

    Add files via upload

---
+++ so_long/libraries/libft/ft_strjoin.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cde-paiv <cde-paiv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/25 20:57:55 by cde-paiv          #+#    #+#             */
+/*   Updated: 2024/04/25 21:33:33 by cde-paiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
+}
+
+/*#include <stdlib.h>
+
+int main()
+{
+    char const *s1 = "Hello, ";
+    char const *s2 = "world!";
+    char *result;
+
+    result = ft_strjoin(s1, s2);
+
+    printf("Resultado: %s\n", result);
+    free(result);
+    return 0;
+}*/
